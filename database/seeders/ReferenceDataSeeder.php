8b0ba24b2f93164ef70e31df8d15a9fb797b0857
Frank <kefoueg@gmail.com>
2026-09-25 11:09:08 +0100

    seeder updated

--- database/seeders/ReferenceDataSeeder.php
+++ database/seeders/ReferenceDataSeeder.php
@@ -28,7 +28,7 @@ class ReferenceDataSeeder extends Seeder
             ['name' => 'Paris CDG', 'type' => LocationType::Airport, 'city' => 'Roissy', 'country' => 'FR', 'lat' => 49.0097, 'lng' => 2.5479],
             ['name' => 'New York JFK', 'type' => LocationType::Airport, 'city' => 'New York', 'country' => 'US', 'lat' => 40.6413, 'lng' => -73.7781],
             ['name' => 'Port du Havre', 'type' => LocationType::Port, 'city' => 'Le Havre', 'country' => 'FR', 'lat' => 49.4944, 'lng' => 0.1079],
-            ['name' => 'Port de Douala', 'type' => LocationType::Port, 'city' => 'Douala', 'country' => 'CM', 'lat' => 4.0511, 'lng' => 9.7679],
+            ['name' => 'Port de Southampton', 'type' => LocationType::Port, 'city' => 'Southampton', 'country' => 'GB', 'lat' => 50.9000, 'lng' => -1.4044],
             ['name' => 'Port de Marseille', 'type' => LocationType::Port, 'city' => 'Marseille', 'country' => 'FR', 'lat' => 43.2965, 'lng' => 5.3698],
             ['name' => 'Entrepôt Logixys', 'type' => LocationType::Warehouse, 'city' => 'Paris', 'country' => 'FR', 'lat' => 48.8300, 'lng' => 2.3700],
         ] as $location) {
